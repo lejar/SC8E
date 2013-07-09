ee0e01795463e2656325d9e9d8df7b3efee026e4
lejar <christopher.pezley@rwth-aachen.de>
2013-07-10 01:00:35 +0200

    Fixed alignment in chip8.h

--- src/chip8.h
+++ src/chip8.h
@@ -41,40 +41,40 @@ public:
   void debugRender();
 
   // opcodes
-  void CLS(std::uint16_t);
-  void RET(std::uint16_t);
-  void JP_A(std::uint16_t);
-  void CALL(std::uint16_t);
-  void SE_VB(std::uint16_t);
-  void SNE_VB(std::uint16_t);
-  void SE_VV(std::uint16_t);
-  void LD_VB(std::uint16_t);
-  void ADD_VB(std::uint16_t);
-  void LD_VV(std::uint16_t);
-  void OR(std::uint16_t);
-  void AND(std::uint16_t);
-  void XOR(std::uint16_t);
-  void ADD_VV(std::uint16_t);
-  void SUB_VV(std::uint16_t);
-  void SHR(std::uint16_t);
-  void SUBN(std::uint16_t);
-  void SHL(std::uint16_t);
-  void SNE_VV(std::uint16_t);
-  void LD_IA(std::uint16_t);
-  void JP_VA(std::uint16_t);
-  void RND(std::uint16_t);
-  void DRW(std::uint16_t);
-  void SKP(std::uint16_t);
-  void SKNP(std::uint16_t);
-  void LD_VDT(std::uint16_t);
-  void LD_VK(std::uint16_t);
-  void LD_DTV(std::uint16_t);
-  void LD_STV(std::uint16_t);
-  void ADD_IV(std::uint16_t);
-  void LD_FV(std::uint16_t);
-  void LD_BV(std::uint16_t);
-  void LD_IV(std::uint16_t);
-  void LD_VI(std::uint16_t);
+  void CLS    (std::uint16_t);
+  void RET    (std::uint16_t);
+  void JP_A   (std::uint16_t);
+  void CALL   (std::uint16_t);
+  void SE_VB  (std::uint16_t);
+  void SNE_VB (std::uint16_t);
+  void SE_VV  (std::uint16_t);
+  void LD_VB  (std::uint16_t);
+  void ADD_VB (std::uint16_t);
+  void LD_VV  (std::uint16_t);
+  void OR     (std::uint16_t);
+  void AND    (std::uint16_t);
+  void XOR    (std::uint16_t);
+  void ADD_VV (std::uint16_t);
+  void SUB_VV (std::uint16_t);
+  void SHR    (std::uint16_t);
+  void SUBN   (std::uint16_t);
+  void SHL    (std::uint16_t);
+  void SNE_VV (std::uint16_t);
+  void LD_IA  (std::uint16_t);
+  void JP_VA  (std::uint16_t);
+  void RND    (std::uint16_t);
+  void DRW    (std::uint16_t);
+  void SKP    (std::uint16_t);
+  void SKNP   (std::uint16_t);
+  void LD_VDT (std::uint16_t);
+  void LD_VK  (std::uint16_t);
+  void LD_DTV (std::uint16_t);
+  void LD_STV (std::uint16_t);
+  void ADD_IV (std::uint16_t);
+  void LD_FV  (std::uint16_t);
+  void LD_BV  (std::uint16_t);
+  void LD_IV  (std::uint16_t);
+  void LD_VI  (std::uint16_t);
 
   // static variables
   // font set - constains the sprites for drawing characters
@@ -121,70 +121,70 @@ public:
   // function list
   std::array<std::map<std::uint16_t, std::function<void(std::uint16_t)>>, 16> opcodes{{
     { // 0x00..
-      {0x0000, std::bind(&chip8::CLS, this, std::placeholders::_1)},
-      {0x000E, std::bind(&chip8::RET, this, std::placeholders::_1)}
+      {0x0000, std::bind(&chip8::CLS,     this, std::placeholders::_1)},
+      {0x000E, std::bind(&chip8::RET,     this, std::placeholders::_1)}
     },
     { // 0x1000
-      {0x1000, std::bind(&chip8::JP_A, this, std::placeholders::_1)}
+      {0x1000, std::bind(&chip8::JP_A,    this, std::placeholders::_1)}
     },
     { // 0x2000
-      {0x2000, std::bind(&chip8::CALL, this, std::placeholders::_1)}
+      {0x2000, std::bind(&chip8::CALL,    this, std::placeholders::_1)}
     },
     { // 0x3000
-      {0x3000, std::bind(&chip8::SE_VB, this, std::placeholders::_1)}
+      {0x3000, std::bind(&chip8::SE_VB,   this, std::placeholders::_1)}
     },
     { // 0x4000
-      {0x4000, std::bind(&chip8::SNE_VB, this, std::placeholders::_1)}
+      {0x4000, std::bind(&chip8::SNE_VB,  this, std::placeholders::_1)}
     },
     { // 0x5000
-      {0x5000, std::bind(&chip8::SE_VV, this, std::placeholders::_1)}
+      {0x5000, std::bind(&chip8::SE_VV,   this, std::placeholders::_1)}
     },
     { // 0x6000
-      {0x6000, std::bind(&chip8::LD_VB, this, std::placeholders::_1)}
+      {0x6000, std::bind(&chip8::LD_VB,   this, std::placeholders::_1)}
     },
     { // 0x7000
-      {0x7000, std::bind(&chip8::ADD_VB, this, std::placeholders::_1)}
+      {0x7000, std::bind(&chip8::ADD_VB,  this, std::placeholders::_1)}
     },
     { // 0x800.
-      {0x0000, std::bind(&chip8::LD_VV, this, std::placeholders::_1)},
-      {0x0001, std::bind(&chip8::OR, this, std::placeholders::_1)},
-      {0x0002, std::bind(&chip8::AND, this, std::placeholders::_1)},
-      {0x0003, std::bind(&chip8::XOR, this, std::placeholders::_1)},
-      {0x0004, std::bind(&chip8::ADD_VV, this, std::placeholders::_1)},
-      {0x0005, std::bind(&chip8::SUB_VV, this, std::placeholders::_1)},
-      {0x0006, std::bind(&chip8::SHR, this, std::placeholders::_1)},
-      {0x0007, std::bind(&chip8::SUBN, this, std::placeholders::_1)},
-      {0x000E, std::bind(&chip8::SHL, this, std::placeholders::_1)}
+      {0x0000, std::bind(&chip8::LD_VV,   this, std::placeholders::_1)},
+      {0x0001, std::bind(&chip8::OR,      this, std::placeholders::_1)},
+      {0x0002, std::bind(&chip8::AND,     this, std::placeholders::_1)},
+      {0x0003, std::bind(&chip8::XOR,     this, std::placeholders::_1)},
+      {0x0004, std::bind(&chip8::ADD_VV,  this, std::placeholders::_1)},
+      {0x0005, std::bind(&chip8::SUB_VV,  this, std::placeholders::_1)},
+      {0x0006, std::bind(&chip8::SHR,     this, std::placeholders::_1)},
+      {0x0007, std::bind(&chip8::SUBN,    this, std::placeholders::_1)},
+      {0x000E, std::bind(&chip8::SHL,     this, std::placeholders::_1)}
     },
     { // 0x9000
-      {0x9000, std::bind(&chip8::SNE_VV, this, std::placeholders::_1)}
+      {0x9000, std::bind(&chip8::SNE_VV,  this, std::placeholders::_1)}
     },
     { // 0xA000
-      {0xA000, std::bind(&chip8::LD_IA, this, std::placeholders::_1)}
+      {0xA000, std::bind(&chip8::LD_IA,   this, std::placeholders::_1)}
     },
     { // 0xB000
-      {0xB000, std::bind(&chip8::JP_VA, this, std::placeholders::_1)}
+      {0xB000, std::bind(&chip8::JP_VA,   this, std::placeholders::_1)}
     },
     { // 0xC000
-      {0xC000, std::bind(&chip8::RND, this, std::placeholders::_1)}
+      {0xC000, std::bind(&chip8::RND,     this, std::placeholders::_1)}
     },
     { // 0xD000
-      {0xD000, std::bind(&chip8::DRW, this, std::placeholders::_1)}
+      {0xD000, std::bind(&chip8::DRW,     this, std::placeholders::_1)}
     },
     { // 0xE0..
-      {0x000E, std::bind(&chip8::SKP, this, std::placeholders::_1)},
-      {0x0001, std::bind(&chip8::SKNP, this, std::placeholders::_1)}
+      {0x000E, std::bind(&chip8::SKP,     this, std::placeholders::_1)},
+      {0x0001, std::bind(&chip8::SKNP,    this, std::placeholders::_1)}
     },
     { // 0xF0..
-      {0x0007, std::bind(&chip8::LD_VDT, this, std::placeholders::_1)},
-      {0x000A, std::bind(&chip8::LD_VK, this, std::placeholders::_1)},
-      {0x0015, std::bind(&chip8::LD_DTV, this, std::placeholders::_1)},
-      {0x0018, std::bind(&chip8::LD_STV, this, std::placeholders::_1)},
-      {0x001E, std::bind(&chip8::ADD_IV, this, std::placeholders::_1)},
-      {0x0029, std::bind(&chip8::LD_FV, this, std::placeholders::_1)},
-      {0x0033, std::bind(&chip8::LD_BV, this, std::placeholders::_1)},
-      {0x0055, std::bind(&chip8::LD_IV, this, std::placeholders::_1)},
-      {0x0065, std::bind(&chip8::LD_VI, this, std::placeholders::_1)}
+      {0x0007, std::bind(&chip8::LD_VDT,  this, std::placeholders::_1)},
+      {0x000A, std::bind(&chip8::LD_VK,   this, std::placeholders::_1)},
+      {0x0015, std::bind(&chip8::LD_DTV,  this, std::placeholders::_1)},
+      {0x0018, std::bind(&chip8::LD_STV,  this, std::placeholders::_1)},
+      {0x001E, std::bind(&chip8::ADD_IV,  this, std::placeholders::_1)},
+      {0x0029, std::bind(&chip8::LD_FV,   this, std::placeholders::_1)},
+      {0x0033, std::bind(&chip8::LD_BV,   this, std::placeholders::_1)},
+      {0x0055, std::bind(&chip8::LD_IV,   this, std::placeholders::_1)},
+      {0x0065, std::bind(&chip8::LD_VI,   this, std::placeholders::_1)}
     }
   }};
 };
